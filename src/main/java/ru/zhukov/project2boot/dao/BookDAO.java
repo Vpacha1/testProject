7b201a4e2095e0eb96c75108c68c1704892255bb
Aleksander <zam1305.01@gmail.com>
2023-11-13 02:36:07 +0400

    Initial commit

---
+++ src/main/java/ru/zhukov/project2boot/dao/BookDAO.java
@@ -0,0 +1,11 @@
+package ru.zhukov.project2boot.dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Neil Alishev
+ */
+@Component
+public class BookDAO {
+    // Здесь будут лежать специфические запросы к БД (с помощью SQL)
+}
